23a1fa8e8b71a7ea5d62d9f31436af14eea1728d
sega <synoecium@google.com>
2023-02-11 17:37:08 +0100

    Linux scripts added with Unix makefiles generator

---
+++ Linux/StaticLib_GCC.sh
@@ -0,0 +1,41 @@
+#!bin/bash
+echo ""
+echo "=================== 1. Download latest wxWidgets sources from Github ==================="
+git clone --recurse-submodules https://github.com/wxWidgets/wxWidgets.git
+if [ $? -ne 0 ]; then
+	cd wxWidgets
+	git pull
+else
+	cd wxWidgets
+fi
+
+
+echo ""
+echo "=================== 2. Build sources into static libraries ==================="
+echo "------------------- 2.1. Generate CMake Makefiles -------------------"
+rm -f "CMakeCache.txt"
+cmake -G "Unix Makefiles" -DwxBUILD_SHARED=OFF -DCMAKE_BUILD_TYPE=Debug
+
+if [ $? -ne 0 ] ; then 
+	exit $? 
+fi
+
+echo ""
+echo "------------------- 2.2. Build sources (debug) -------------------"
+
+cmake --build .
+if [ $? -ne 0 ] ; then 
+	exit $? 
+fi
+
+echo ""
+echo "------------------- 2.3. Build sources (release) -------------------"
+cmake -G "Unix Makefiles" -DwxBUILD_SHARED=OFF -DwxBUILD_DEMOS=ON -DCMAKE_BUILD_TYPE=Release
+cmake --build .
+if [ $? -ne 0 ] ; then 
+	exit $? 
+fi
+
+echo ""
+echo "=================== BUILD SUCCESSFUL! ==================="
+echo ""
